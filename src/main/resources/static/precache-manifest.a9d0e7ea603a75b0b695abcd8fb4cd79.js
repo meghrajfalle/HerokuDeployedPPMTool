8b25646dd847478f5cfbbb1bc47c5d60fc4653ae
Meghraj Falle <meghraj.falle@PUMFALLE.local>
2020-03-09 07:45:06 +0530

    React Build files added

---
+++ src/main/resources/static/precache-manifest.a9d0e7ea603a75b0b695abcd8fb4cd79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca35f74b2e85d325b3f06da7d9a0a23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9ceac0b7065307b640b",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ad04dd8c8f777de80ada",
+    "url": "/static/css/main.57177e8f.chunk.css"
+  },
+  {
+    "revision": "b9ceac0b7065307b640b",
+    "url": "/static/js/2.9d07c27c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.9d07c27c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad04dd8c8f777de80ada",
+    "url": "/static/js/main.d3509877.chunk.js"
+  },
+  {
+    "revision": "8677cb06ee07da3e0258",
+    "url": "/static/js/runtime-main.f12de98b.js"
+  }
+]);
